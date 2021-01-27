a65ce6941fad626cdb1d905a820d5ac7fdc98420
otherPOM <otherPOM@gmail.com>
2021-01-27 21:14:12 +0300

    Stage 1 done

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
 package correcter;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var line = scan.nextLine();
+        var res = new StringBuilder();
+        var rand = new Random();
+
+        for (int i = 0; i < line.length(); i += 3) {
+            var sub = line.substring(i, Math.min(i + 3, line.length())).toCharArray();
+            sub[rand.nextInt(Math.min(3, sub.length))] = (char) (rand.nextInt(27) + 'a' - 1);
+            res.append(sub);
+        }
+
+        System.out.println(res);
     }
 }
