6d0130af5e3e1a0e891412aef1ecbc354840866a
otherPOM <otherPOM@gmail.com>
2021-01-29 13:14:51 +0300

    Stage 2 done

--- Main.java
+++ Main.java
@@ -1,22 +1,59 @@
 package correcter;
 
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scan = new Scanner(System.in);
+    private static final char[] symbolsNumbersSpace;
+
+    static {
+        var s = "abcdefghijklmnopqrstuvwxyz";
+        symbolsNumbersSpace = (s + s.toUpperCase(Locale.ROOT) + "0123456789 ").toCharArray();
+    }
 
     public static void main(String[] args) {
         var line = scan.nextLine();
+        var encoded = encode(line);
+        var corrupted = simulateCorruption(encoded);
+        var decoded = decode(corrupted);
+        System.out.println(line);
+        System.out.println(encoded);
+        System.out.println(corrupted);
+        System.out.println(decoded);
+    }
+
+    private static String simulateCorruption(String data) {
         var res = new StringBuilder();
         var rand = new Random();
 
-        for (int i = 0; i < line.length(); i += 3) {
-            var sub = line.substring(i, Math.min(i + 3, line.length())).toCharArray();
-            sub[rand.nextInt(Math.min(3, sub.length))] = (char) (rand.nextInt(27) + 'a' - 1);
+        for (int i = 0; i < data.length(); i += 3) {
+            var sub = data.substring(i, Math.min(i + 3, data.length())).toCharArray();
+            var corruption = symbolsNumbersSpace[rand.nextInt(symbolsNumbersSpace.length)];
+            sub[rand.nextInt(Math.min(3, sub.length))] = corruption;
             res.append(sub);
         }
 
-        System.out.println(res);
+        return res.toString();
+    }
+
+    private static String encode(String data) {
+        var sb = new StringBuilder();
+        for (char c : data.toCharArray()) {
+            sb.append(c).append(c).append(c);
+        }
+        return sb.toString();
+    }
+
+    private static String decode(String data) {
+        var sb = new StringBuilder();
+        for (int i = 0; i < data.length(); i += 3) {
+            var c1 = data.charAt(i);
+            var c2 = data.charAt(i + 1);
+            var c3 = data.charAt(i + 2);
+            sb.append(c1 == c2 ? c1 : c1 == c3 ? c1 : c2);
+        }
+        return sb.toString();
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 ## Stage 1
 In this stage, you should write a program that creates errors in the input text, 1 random error per 3 symbols. An error means that the character is replaced by another random character. For example, “abc” characters can be “*bc” or “a*c” or “ab*”, where * is a random character. You can replace it with any character but it's recommended to use only uppercase and lowercase English letters, spacebar, and numbers.
 
+## Stage 2
+In this stage, you should write a program that:
+
+1. Takes a message the user wants it to send. The input contains a single message.
+2. Encode the message by tripling all the symbols.
+3. Simulate sending this message via a poor internet connection (in other words, simulate errors).
+4. Decode it back again.
